a65edc3514bcfdcd9b044ca797f3ec18eb19d4ee
Philipp Hager <e0803285@gmail.com>
2013-11-23 15:02:07 +0100

    #1028 disable id-number which is not used in grouptool

--- lib.php
+++ lib.php
@@ -52,7 +52,7 @@ function grouptool_supports($feature) {
         case FEATURE_GROUPINGS:         return true;
         case FEATURE_GROUPMEMBERSONLY:  return true;
         case FEATURE_BACKUP_MOODLE2:    return true;
-        case FEATURE_IDNUMBER:          return true;
+        case FEATURE_IDNUMBER:          return false;
         case FEATURE_MOD_INTRO:         return true;
         case FEATURE_SHOW_DESCRIPTION:  return true;
         case FEATURE_MOD_ARCHETYPE:     return MOD_ARCHETYPE_OTHER;
