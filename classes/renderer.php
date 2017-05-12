e6ff9b4762ac9f7078f80dd5b43dc511d6ebb54f
Philipp Hager <office@phager.at>
2017-05-12 16:44:18 +0200

    #4076 use mustache templates for sortlist

--- classes/renderer.php
+++ classes/renderer.php
@@ -51,197 +51,25 @@ class renderer extends \plugin_renderer_base {
             return $this->get_no_groups_info($sortlist);
         }
 
-        // Generate draggable items - each representing 1 group!
-        $moveupstr = get_string('moveup', 'grouptool');
-        $movedownstr = get_string('movedown', 'grouptool');
-        $dragstr = get_string('drag', 'grouptool');
-        $activestr = get_string('active');
-        $inactivestr = get_string('inactive');
-        $deletestr = get_string('delete');
-        $renamestr = get_string('rename');
-        $resizestr = get_string('resize', 'grouptool');
-        $table = new \html_table();
-        $table->data = array();
-        $table->attributes['class'] .= ' table table-striped ';
-        $table->head = array( 0 => '',
-                              1 => '',
-                              2 => get_string('name'));
-        if (!empty($sortlist->usesize)) {
-            $table->head[3] = get_string('size');
-        }
-        $groupstatus = get_string('groupstatus', 'grouptool').$OUTPUT->help_icon('groupstatus', 'grouptool');
-        $table->head = $table->head + array(4 => $groupstatus,
-                                            5 => '',
-                                            6 => '',
-                                            7 => '' );
         foreach ($sortlist->groups as $id => $group) {
-            $row = array(); // Each group gets its own row!
-
-            $classes = array('checkbox_status', 'class0');
-            if (!empty($group->groupings) && (count($group->groupings) > 0)) {
-                foreach (array_keys($group->groupings) as $groupingid) {
-                    $classes[] .= 'class'.$groupingid;
-                }
-            }
-
-            $chkboxattr = array(
-                    'name'  => 'selected[]',
-                    'type'  => 'checkbox',
-                    'class' => implode(' ', $classes),
-                    'value' => $id);
-            if (!empty($sortlist->selected[$id])) {
-                $chkboxattr['checked'] = 'checked';
-            } else if (isset($chkboxattr['checked'])) {
-                unset($chkboxattr['checked']);
-            }
-
-            $hiddenattr = array(
-                    'name'  => 'order['.$id.']',
-                    'type'  => 'hidden',
-                    'value' => (!empty($group->order) ? $group->order : 999999),
-                    'class' => 'sort_order');
-
-            $moveupattr = array('title' => $moveupstr,
-                                'name'  => 'moveup['.$id.']',
-                                'class' => 'moveupbutton');
-            $moveupicon = $OUTPUT->pix_icon('i/up', $moveupstr, 'moodle', $moveupattr);
-            $moveupurl = new \moodle_url($PAGE->url, array('moveup' => $id));
-            $moveupbutton = \html_writer::link($moveupurl, $moveupicon, $moveupattr);
-            $movedownattr = array('title' => $movedownstr,
-                                  'name'  => 'movedown['.$id.']',
-                                  'class' => 'movedownbutton');
-            $movedownicon = $OUTPUT->pix_icon('i/down', $movedownstr, 'moodle', $movedownattr);
-            $movedownurl = new \moodle_url($PAGE->url, array('movedown' => $id));
-            $movedownbutton = \html_writer::link($movedownurl, $movedownicon, $movedownattr);
-            $dragbutton = $OUTPUT->pix_icon('i/dragdrop', $dragstr, 'moodle', array('title' => $dragstr,
-                                                                                    'class' => 'drag_image js_invisible'));
-            $nameattr = array('name'  => 'name['.$id.']',
-                              'type'  => 'hidden',
-                              'value' => $group->name);
-            $nameblock = \html_writer::tag('span', $group->name, array('class' => 'text')).
-                         \html_writer::empty_tag('input', $nameattr);
-            $renameattr = array('title' => $renamestr,
-                                'type'  => 'image',
-                                'name'  => 'rename['.$id.']',
-                                'class' => 'renamebutton');
-            $renamebutton = \html_writer::link(new \moodle_url($PAGE->url, array('rename' => $id)),
-                                               $OUTPUT->pix_icon('t/editstring', $renamestr, 'moodle', $renameattr),
-                                               array('class' => $renameattr['class'], 'title' => $renamestr));
-            $nameblock .= $renamebutton;
-
-            $drag = new \html_table_cell($dragbutton);
-            $drag->attributes['class'] = 'buttons';
-
-            $deleteattr = array('title' => $deletestr,
-                                'name'  => 'delete['.$id.']',
-                                'class' => 'deletebutton',
-                                'id'    => 'delete_'.$id);
-            $deletebutton = \html_writer::link(new \moodle_url($PAGE->url, array('delete' => $id)),
-                                               $OUTPUT->pix_icon('t/delete', $deletestr, 'moodle', $deleteattr),
-                                               array('class' => $deleteattr['class'], 'title' => $deletestr));
-
-            $row = array( 0 => new \html_table_cell(\html_writer::empty_tag('input', $chkboxattr)),
-                          1 => $drag,
-                          2 => new \html_table_cell($nameblock.
-                                                   \html_writer::empty_tag('input', $hiddenattr)));
-            $row[0]->attributes['class'] = 'checkbox_container';
-            $row[2]->attributes['class'] = 'grpname';
-
-            if (!empty($sortlist->usesize)) {
-                $sizeattr = array('name'  => 'size['.$id.']',
-                                  'type'  => 'hidden',
-                                  'value' => clean_param($group->size, PARAM_INT));
-                $sizeblock = \html_writer::tag('span', $group->size, array('class' => 'text')).
-                             \html_writer::empty_tag('input', $sizeattr);
-                $resizeattr = array('id'    => 'resize_'.$id,
-                                    'title' => $resizestr,
-                                    'type'  => 'image',
-                                    'name'  => 'resize['.$id.']',
-                                    'class' => 'resizebutton');
-                $resizebutton = \html_writer::link(new \moodle_url($PAGE->url, array('resize' => $id)),
-                                                   $OUTPUT->pix_icon('t/editstring', $resizestr, 'moodle', $resizeattr),
-                                                   array('class' => $resizeattr['class'], 'title' => $resizestr));
-                $sizeblock .= $resizebutton;
-
-                $fieldcell = new \html_table_cell($sizeblock);
-                $fieldcell->attributes['class'] = "size addfield";
-                $row[] = $fieldcell;
-            }
-
-            if ($group->status) {
-                $toggleattr = array('title' => $activestr,
-                                    'type'  => 'active',
-                                    'name'  => 'toggle['.$id.']',
-                                    'class' => 'active');
-                $toggleicon = $OUTPUT->pix_icon('active', $activestr, 'mod_grouptool', $toggleattr);
-            } else {
-                $toggleattr = array('title' => $inactivestr,
-                                    'type'  => 'image',
-                                    'name'  => 'toggle['.$id.']',
-                                    'class' => 'inactive');
-                $toggleicon = $OUTPUT->pix_icon('inactive', $inactivestr, 'mod_grouptool', $toggleattr);
-            }
-            $linkattr = array('class' => 'togglebutton '.$toggleattr['class'],
-                              'title' => $toggleattr['title']);
-            $togglebutton = \html_writer::link(new \moodle_url($PAGE->url, array('toggle' => $id)), $toggleicon, $linkattr);
-            unset($linkattr);
-            $toggle = new \html_table_cell($togglebutton);
-            $toggle->attributes['class'] = 'buttons';
-            $row[] = $toggle;
-            $moveup = new \html_table_cell($moveupbutton);
-            $moveup->attributes['class'] = 'buttons';
-            $row[] = $moveup;
-            $movedown = new \html_table_cell($movedownbutton);
-            $movedown->attributes['class'] = 'buttons';
-            $row[] = $movedown;
-            $delete = new \html_table_cell($deletebutton);
-            $delete->attributes['class'] = 'buttons';
-            $row[] = $delete;
-
-            $row = new \html_table_row($row);
-            $row->attributes['class'] = 'draggable_item';
-            if (!$group->status) {
-                $row->attributes['class'] .= ' dimmed_text';
-            }
-
-            if ($group->status == null) {
-                $row->attributes['class'] .= ' missing_agrp';
-            }
-
-            $rows[] = $row;
+            $sortlist->groups[$id]->checked = !empty($sortlist->selected[$id]);
+            $sortlist->groups[$id]->missing = ($group->status == null);
+            // Ensure the keys are right!
+            $sortlist->groups[$id]->groupings = array_values($group->groupings);
+            $sortlist->groups[$id]->id = $id;
+            $sortlist->groups[$id]->order = !empty($group->order) ? $group->order : 999999;
         }
 
-        $table->data = $rows;
-        $table->attributes['class'] .= ' drag_list ';
+        $context = new stdClass();
+        $context->usesize = $sortlist->usesize;
+        $context->pageurl = $PAGE->url->out();
+        $url = new \moodle_url('/course/modedit.php', array('update' => $sortlist->cm->id, 'return' => 1));
+        $context->courseediturl = $url->out();
+        $statushelpicon = new \help_icon('groupstatus', 'grouptool');
+        $context->statushelpicon = $statushelpicon->export_for_template($OUTPUT);
+        $context->groups = array_values($sortlist->groups);
 
-        $controller = \html_writer::link(new \moodle_url($PAGE->url, array('class_action' => 'select',
-                                                                           'do_class_action' => '1')),
-                                        get_string('all'),
-                                        array('class' => 'simple_select_all')).
-                      '/'.
-                      \html_writer::link(new \moodle_url($PAGE->url, array('class_action' => 'deselect',
-                                                                           'do_class_action' => '1')),
-                                        get_string('none'),
-                                        array('class' => 'simple_select_none'));
-        if ($sortlist->usesize) {
-            $settingsurl = new \moodle_url('/course/modedit.php', array('update' => $sortlist->cm->id, 'return' => 1));
-            $linkarg = array('class' => 'text-info text-right pull-right');
-            $controller = \html_writer::tag('span', $controller, array('class' => 'text-left')).
-                          \html_writer::link($settingsurl, get_string('individual_size_info', 'grouptool'), $linkarg);
-            unset($linkarg);
-        }
-
-        $controller = \html_writer::tag('div', $controller);
-
-        $tablehtml = $controller.\html_writer::table($table).$controller;
-
-        if (count($sortlist->groups)) {
-            $content = \html_writer::tag('div', $tablehtml, array('class' => 'drag_area'));
-        } else {
-            $content = $this->get_no_groups_info($sortlist);
-        }
-
-        $html = \html_writer::tag('div', $content, array('class' => 'fitem sortlist_container'));
+        $html = $OUTPUT->render_from_template('mod_grouptool/sortlist', $context);
 
         // Init JS!
         $context = \context_module::instance($sortlist->cm->id);
