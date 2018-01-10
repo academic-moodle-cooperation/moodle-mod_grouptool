2a3eca0affc09c58b6244ebeba2e28870a4736b7
Philipp Hager <office@phager.at>
2018-01-10 15:43:03 +0100

    #4844 update README, CHANGELOG and travis.yml and bump version number to 2018011000 release v3.4.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081002;
-$plugin->requires  = 2017051500;
+$plugin->version   = 2018011000;
+$plugin->requires  = 2017111300;
 $plugin->component = 'mod_grouptool';
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = "v3.4-dev";
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = "v3.4.0";
