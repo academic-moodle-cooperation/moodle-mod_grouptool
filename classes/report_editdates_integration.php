21260c8782d8d3445ee91f70872ee5c1900f4d46
Hannes Laimer <hannes.laimer@rolmail.net>
2019-06-12 20:57:30 +0200

    added report-editdates support #6128

---
+++ classes/report_editdates_integration.php
@@ -0,0 +1,73 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die;
+
+
+require_once($CFG->dirroot.'/mod/grouptool/locallib.php');
+/**
+ * Class needed for report-editdates support
+ *
+ * @package       mod_grouptool
+ * @author        Andreas Krieger
+ * @copyright     2019 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_grouptool_report_editdates_integration
+extends report_editdates_mod_date_extractor {
+
+    public function __construct($course) {
+        parent::__construct($course, 'grouptool');
+        parent::load_data();
+    }
+
+    public function get_settings(cm_info $cm) {
+        $grouptool = $this->mods[$cm->instance];
+
+        return array(
+                'timeavailable' => new report_editdates_date_setting(
+                        get_string('availabledate', 'grouptool'),
+                        $grouptool->timeavailable,
+                        self::DATETIME, true, 5),
+                'timedue' => new report_editdates_date_setting(
+                        get_string('duedate', 'grouptool'),
+                        $grouptool->timedue,
+                        self::DATETIME, true, 5),
+                );
+    }
+
+    public function validate_dates(cm_info $cm, array $dates) {
+        $errors = array();
+        if (!empty($dates['timedue']) && ($dates['timedue'] <= $dates['timeavailable'])) {
+            $errors['timedue'] = get_string('determinismerror', 'grouptool');
+        }
+
+        return $errors;
+    }
+
+    public function save_dates(cm_info $cm, array $dates) {
+        global $DB, $COURSE, $CFG;
+
+        $update = new stdClass();
+        $update->id = $cm->instance;
+        $update->timedue = $dates['timedue'];
+        $update->timeavailable = $dates['timeavailable'];
+
+        $result = $DB->update_record('grouptool', $update);
+
+        grouptool_refresh_events(0, $cm->instance);
+    }
+}
