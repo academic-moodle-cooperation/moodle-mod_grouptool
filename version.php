c33d6d50ab76b5a50b15348519ba672af2f7ce27
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2022-07-27 07:06:08 +0000

    #7203 Update to 3.11.3

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021051902;
+$plugin->version   = 2021051903;
 $plugin->requires  = 2021051700;
 $plugin->component = 'mod_grouptool';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.11.2";
+$plugin->release   = "v3.11.3";
