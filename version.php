92b92790fbad63fd292def34817bc84cb4cf182b
Daniel Binder <daniel.binder@univie.ac.at>
2022-10-12 15:54:15 +0200

    #7317 Update to 3.11.4

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021051903;
+$plugin->version   = 2021051904;
 $plugin->requires  = 2021051700;
 $plugin->component = 'mod_grouptool';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.11.3";
+$plugin->release   = "v3.11.4";
