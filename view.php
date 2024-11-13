29df1b6f333ec585799ab28c611f3913bfdf9b80
annek <annek03@univie.ac.at>
2024-11-13 12:07:28 +0100

    7537 Start splitting files from tabtree into different files to skip tab parameter

--- view.php
+++ view.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-global $SESSION, $OUTPUT;
+global $SESSION, $OUTPUT, $CFG, $DB, $USER, $PAGE;
 
 /**
  * Displays a particular instance of mod_grouptool
@@ -34,7 +34,6 @@ global $SESSION, $OUTPUT;
  * @copyright 2024 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 // TODO split tabs in different PHP files and define it only when really needed (=import-tab , needed for progress bar)!
 // @codingStandardsIgnoreLine
 if ((isset($_POST['tab']) && $_POST['tab'] === 'import') || (isset($_GET['tab']) && $_GET['tab'] === 'import')
@@ -43,26 +42,25 @@ if ((isset($_POST['tab']) && $_POST['tab'] === 'import') || (isset($_GET['tab'])
     define('NO_OUTPUT_BUFFERING', true);
 // @codingStandardsIgnoreLine
 }
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once($CFG->dirroot.'/mod/grouptool/locallib.php');
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+require_once($CFG->dirroot . '/mod/grouptool/locallib.php');
 
 // Do we get course_module ID?
 $id = optional_param('id', 0, PARAM_INT);
 // Or do we get grouptool instance ID?
-$g  = optional_param('g', 0, PARAM_INT);
+$g = optional_param('g', 0, PARAM_INT);
 
 if ($id) {
-    $cm         = get_coursemodule_from_id('grouptool', $id, 0, false, MUST_EXIST);
-    $course     = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
-    $grouptool  = $DB->get_record('grouptool', ['id' => $cm->instance], '*', MUST_EXIST);
+    $cm = get_coursemodule_from_id('grouptool', $id, 0, false, MUST_EXIST);
+    $course = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
+    $grouptool = $DB->get_record('grouptool', ['id' => $cm->instance], '*', MUST_EXIST);
 } else if ($g) {
-    $grouptool  = $DB->get_record('grouptool', ['id' => $g], '*', MUST_EXIST);
-    $course     = $DB->get_record('course', ['id' => $grouptool->course], '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('grouptool', $grouptool->id, $course->id, false, MUST_EXIST);
+    $grouptool = $DB->get_record('grouptool', ['id' => $g], '*', MUST_EXIST);
+    $course = $DB->get_record('course', ['id' => $grouptool->course], '*', MUST_EXIST);
+    $cm = get_coursemodule_from_instance('grouptool', $grouptool->id, $course->id, false, MUST_EXIST);
     $id = $cm->id;
 } else {
     print_error('invalidcoursemodule');
-
 }
 
 
@@ -78,6 +76,8 @@ $PAGE->set_context($context);
 $PAGE->set_title(format_string($grouptool->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_activity_record($grouptool);
+$PAGE->add_body_class('mediumwidth');
+
 
 $instance = new mod_grouptool($cm->id, $grouptool, $cm, $course);
 
@@ -97,156 +97,39 @@ $creategrps = has_capability('mod/grouptool:create_groups', $context);
 $creategrpgs = has_capability('mod/grouptool:create_groupings', $context);
 $admingrps = has_capability('mod/grouptool:administrate_groups', $context);
 
-if ($creategrps || $creategrpgs || $admingrps) {
-
-    if ($creategrps && ($admingrps || $creategrpgs)) {
-        $row['administration'] = new tabobject('administration',
-                                               $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                               '&amp;tab=group_admin',
-                                               get_string('administration', 'grouptool'),
-                                               get_string('administration_alt', 'grouptool'),
-                                               false);
-        $row['administration']->subtree['group_admin'] = new tabobject('group_admin',
-                                                                       $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                                                       '&amp;tab=group_admin',
-                                                                       get_string('group_administration', 'grouptool'),
-                                                                       get_string('group_administration_alt', 'grouptool'),
-                                                                       false);
-        $row['administration']->subtree['group_creation'] = new tabobject('group_creation',
-                                                                       $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                                                       '&amp;tab=group_creation',
-                                                                       get_string('group_creation', 'grouptool'),
-                                                                       get_string('group_creation_alt', 'grouptool'),
-                                                                       false);
-    } else if ($creategrps) {
-        $row['administration'] = new tabobject('administration',
-                                               $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                               '&amp;tab=group_admin',
-                                               get_string('administration', 'grouptool'),
-                                               get_string('administration_alt', 'grouptool'),
-                                               false);
-        $row['administration']->subtree['group_creation'] = new tabobject('group_creation',
-                                                                       $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                                                       '&amp;tab=group_creation',
-                                                                       get_string('group_creation', 'grouptool'),
-                                                                       get_string('group_creation_alt', 'grouptool'),
-                                                                       false);
-    } else if ($creategrpgs || $admingrps) {
-        $row['administration'] = new tabobject('administration',
-                                               $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                               '&amp;tab=group_admin',
-                                               get_string('administration', 'grouptool'),
-                                               get_string('administration_alt', 'grouptool'),
-                                               false);
-        $row['administration']->subtree['group_admin'] = new tabobject('group_admin',
-                                                                       $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                                                       '&amp;tab=group_admin',
-                                                                       get_string('group_administration', 'grouptool'),
-                                                                       get_string('group_administration_alt', 'grouptool'),
-                                                                       false);
-    }
-}
-
-if (has_capability('mod/grouptool:register_students', $context)
-        || has_capability('mod/grouptool:register', $context)
-        || has_capability('mod/grouptool:view_own_registration', $context)
-        || has_capability('mod/grouptool:view_groups', $context)) {
-    $row['selfregistration'] = new tabobject('selfregistration',
-                                             $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.
-                                             '&amp;tab=selfregistration',
-                                             get_string('selfregistration', 'grouptool'),
-                                             get_string('selfregistration_alt', 'grouptool'),
-                                             false);
-}
-if (has_capability('mod/grouptool:register_students', $context)
-        || has_capability('mod/grouptool:unregister_students', $context)) {
-    $importtab = new tabobject('import',
-        $CFG->wwwroot . '/mod/grouptool/view.php?id=' . $id . '&amp;tab=import',
-        get_string('import', 'grouptool'),
-        get_string('import_desc', 'grouptool'),
-        false);
-    $unregtab = new tabobject('unregister',
-        $CFG->wwwroot . '/mod/grouptool/view.php?id=' . $id . '&amp;tab=unregister',
-        get_string('unregister', 'grouptool'),
-        get_string('unregister_desc', 'grouptool'),
-        false);
-    if (has_capability('mod/grouptool:register_students', $context)
-        && has_capability('mod/grouptool:unregister_students', $context)) { // Can unregister and register.
-        $row['multiple'] = new tabobject('multiple',
-            $CFG->wwwroot . '/mod/grouptool/view.php?id=' . $id . '&amp;tab=import',
-            get_string('multiple', 'grouptool'),
-            get_string('multiple_desc', 'grouptool'),
-            false);
-        $row['multiple']->subtree['import'] = $importtab;
-        $row['multiple']->subtree['unregister'] = $unregtab;
-    } else if (has_capability('mod/grouptool:register_students', $context)) { // Can only register.
-        $row['import'] = $importtab;
-    } else { // Can only unregister.
-        $row['unregister'] = $unregtab;
-    }
-}
-if (has_capability('mod/grouptool:view_regs_group_view', $context)) {
-    $row['overview'] = new tabobject('overview',
-                                  $CFG->wwwroot.'/mod/grouptool/view.php?id='.$id.'&amp;tab=overview',
-                                  get_string('users_tab', 'grouptool'),
-                                  get_string('users_tab_alt', 'grouptool'),
-                                  false);
-}
-
-
-
 if (!isset($SESSION->mod_grouptool)) {
     $SESSION->mod_grouptool = new stdClass();
 }
 
-// We won't iterate recursively over it right now, if we'll get more levels, we'll have to!
-
-$availabletabs = array_keys($row);
-foreach ($availabletabs as $cur) {
-    if (!empty($row[$cur]->subtree)) {
-        $availabletabs = array_merge($availabletabs, array_keys($row[$cur]->subtree));
-    }
-}
-
-
-
 $modinfo = get_fast_modinfo($course);
 $cm = $modinfo->get_cm($cm->id);
+
 if (empty($cm->uservisible)) {
     $SESSION->mod_grouptool->currenttab = 'conditions_prevent_access';
     $tab = 'conditions_prevent_access';
-} else if (count($row) > 1) {
+    // TODO USE RIGHT CAPABILITIES HERE
+} else if ($creategrps || $creategrpgs || $admingrps) {
     $tab = optional_param('tab', null, PARAM_ALPHAEXT);
     if ($tab) {
         $SESSION->mod_grouptool->currenttab = $tab;
+    }else{
+        $SESSION->mod_grouptool->currenttab = 'default';
     }
 
     if (!isset($SESSION->mod_grouptool->currenttab)
-            || ($SESSION->mod_grouptool->currenttab == 'noaccess')
-            || ($SESSION->mod_grouptool->currenttab == 'conditions_prevent_access')
-            || !in_array($SESSION->mod_grouptool->currenttab, $availabletabs)) {
+        || ($SESSION->mod_grouptool->currenttab == 'noaccess')
+        || ($SESSION->mod_grouptool->currenttab == 'conditions_prevent_access')) {
         // Set standard-tab according to users capabilities!
         if (has_capability('mod/grouptool:create_groupings', $context)
-                || has_capability('mod/grouptool:administrate_groups', $context)) {
+            || has_capability('mod/grouptool:administrate_groups', $context)) {
             $SESSION->mod_grouptool->currenttab = 'group_admin';
         } else if (has_capability('mod/grouptool:create_groups', $context)) {
             $SESSION->mod_grouptool->currenttab = 'group_creation';
         } else if (has_capability('mod/grouptool:register_students', $context)
-                       || has_capability('mod/grouptool:register', $context)) {
+            || has_capability('mod/grouptool:register', $context)) {
             $SESSION->mod_grouptool->currenttab = 'selfregistration';
-        } else {
-            $SESSION->mod_grouptool->currenttab = current($availabletabs);
         }
     }
-
-    mod_grouptool::translate_top_level_tabs($row, $tab);
-
-    $outputcache = $OUTPUT->tabtree($row, $SESSION->mod_grouptool->currenttab, $inactive);
-} else if (count($row) == 1) {
-    $SESSION->mod_grouptool->currenttab = current($availabletabs);
-    $tab = current($availabletabs);
-
-    mod_grouptool::translate_top_level_tabs($row, $tab);
 } else {
     $SESSION->mod_grouptool->currenttab = 'noaccess';
     $tab = 'noaccess';
@@ -257,15 +140,14 @@ $PAGE->url->param('tab', $SESSION->mod_grouptool->currenttab);
 $tab = $SESSION->mod_grouptool->currenttab; // Shortcut!
 
 
-
 /* TRIGGER THE VIEW EVENT */
 $event = \mod_grouptool\event\course_module_viewed::create([
-        'objectid' => $cm->instance,
-        'context'  => context_module::instance($cm->id),
-        'other'    => [
+    'objectid' => $cm->instance,
+    'context' => context_module::instance($cm->id),
+    'other' => [
         'tab' => $tab,
         'name' => $instance->get_name(),
-        ],
+    ],
 ]);
 $event->add_record_snapshot('course', $course);
 // In the next line you can use $PAGE->activityrecord if you have set it, or skip this line if you don't have a record.
@@ -273,30 +155,47 @@ $event->add_record_snapshot($PAGE->cm->modname, $grouptool);
 $event->trigger();
 /* END OF VIEW EVENT */
 
+$node = $PAGE->secondarynav->find_active_node();
+if ($node) {
+    $node->make_inactive();
+
+    switch ($tab){
+        case 'default':
+        case 'selfregistration':
+        case 'noaccess':
+        case 'condition_prevent_access':
+            $node2 = $PAGE->secondarynav->find("modulepage", null);
+            break;
+        case 'group_admin':
+        case 'group_creation':
+            $node2 = $PAGE->secondarynav->find("mod_grouptool_administration", navigation_node::TYPE_SETTING);
+            break;
+        case 'overview':
+        case 'import':
+        case 'unregister_user':
+            $node2 = $PAGE->secondarynav->find("mod_grouptool_registration", navigation_node::TYPE_SETTING);
+            break;
+        default:
+           $node2 = false;
+    }
+    if($node2){
+        $node2->make_active();
+    }
+}
+
 if ($tab != 'selfregistration') {
     // Output starts here!
-
+    echo $OUTPUT->header();
+    echo $outputcache;
 }
 
 switch ($tab) {
+    case 'default':
+        break;
     case 'group_admin':
-        // Update nav fitting to right menu point
-        $node = $PAGE->secondarynav->find_active_node();
-        $node->make_inactive();
-        $node2 = $PAGE->secondarynav->find("mod_grouptool_administration",navigation_node::TYPE_SETTING);
-        $node2->make_active();
-        echo $OUTPUT->header();
-        echo $outputcache;
         $instance->view_administration();
         break;
     case 'overview':
-        // Update nav fitting to right menu point
-        $node = $PAGE->secondarynav->find_active_node();
-        $node->make_inactive();
-        $node2 = $PAGE->secondarynav->find("mod_grouptool_registration",navigation_node::TYPE_SETTING);
-        $node2->make_active();
-        echo $OUTPUT->header();
-        echo $outputcache;
         $instance->view_overview();
         break;
     case 'group_creation':
@@ -321,12 +220,12 @@ switch ($tab) {
             // User cannot access the activity, but on the course page they will
             // see a link to it, greyed-out, with information (HTML format) from
             // $cm->availableinfo about why they can't access it.
-            $text = "<br />".format_text($cm->availableinfo, FORMAT_HTML);
+            $text = "<br />" . format_text($cm->availableinfo, FORMAT_HTML);
         } else {
             // User cannot access the activity and they will not see it at all.
             $text = '';
         }
-        $notification = $OUTPUT->notification(get_string('conditions_prevent_access', 'grouptool').$text, 'error');
+        $notification = $OUTPUT->notification(get_string('conditions_prevent_access', 'grouptool') . $text, 'error');
         echo $OUTPUT->box($notification, 'generalbox centered');
         break;
     default:
