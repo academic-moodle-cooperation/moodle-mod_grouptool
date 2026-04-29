919ea261fa997435d4207dca6c2215380f704244
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-04-29 09:37:43 +0200

    8614: Add overview page and upate some files

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026012100.02;
+$plugin->version   = 2026012100.04;
 $plugin->requires  = 2025100600;
 $plugin->component = 'mod_grouptool';
 $plugin->maturity  = MATURITY_STABLE;
