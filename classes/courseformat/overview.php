d4cf080a5f3b0718ca7681889b9e63e6c47af575
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-04-29 11:06:11 +0200

    8614: Add overview items

--- classes/courseformat/overview.php
+++ classes/courseformat/overview.php
@@ -11,10 +11,27 @@ use core_courseformat\local\overview\overviewitem;
 use core_courseformat\output\local\overview\overviewaction;
 use core_string_manager;
 use mod_grouptool\dates;
-
+/**
+ * Grouptool overview integration.
+ *
+ * @package    mod_grouptool
+ * @copyright  2026 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class overview extends activityoverviewbase {
+    /** @var \mod_grouptool $grouptool the grouptool instance. */
     private \mod_grouptool $grouptool;
 
+    /**
+     * Constructor.
+     *
+     * @param cm_info $cm
+     * @param renderer_helper $rendererhelper
+     * @param core_string_manager $stringmanager
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
     public function __construct(
         cm_info $cm,
         protected readonly renderer_helper $rendererhelper,
@@ -74,11 +91,17 @@ class overview extends activityoverviewbase {
     #[\Override]
     public function get_extra_overview_items(): array {
         return [
-            'registrtaionstatus' => $this->get_extra_registration_overview(),
+            'registrationoverview' => $this->get_extra_registration_overview(),
+            'regstrationstatus' => $this->get_extra_registration_status(),
         ];
     }
     /**
-     *
+     * Returns an overview item with the count of registered students for this activity
+     * Checks if the user has the capability to view the registration overview and then returns the count of registered students and total students.
+     * @return overviewitem|null
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \required_capability_exception
      */
     private function get_extra_registration_overview(): ?overviewitem {
         global $USER;
@@ -95,8 +118,41 @@ class overview extends activityoverviewbase {
             content: get_string(
                 'count_of_total',
                 'core',
-                ['count' => $registrations->occupied_places, 'total' => $registrations->users]
+                ['count' => $registrations->reg_users, 'total' => $registrations->users]
             ),
+        );
+    }
+
+    /**
+     * Returns the registration status of the user for this activity
+     * Checks if the user has the capability to register and then checks if the user is registered or not.
+     * @return overviewitem|null
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \required_capability_exception
+     */
+    private function get_extra_registration_status(): ?overviewitem {
+        global $USER;
+
+        if (!has_capability('mod/grouptool:register', $this->context)) {
+            return null;
+        }
+
+        $registration = $this->grouptool->get_user_reg_count($USER->id);
+        [,$min,]=$this->grouptool->get_reg_settings();
+
+        if ($registration >= $min && $registration > 0) {
+            return new overviewitem(
+                name: get_string('registration_details', 'grouptool'),
+                value: true,
+                content:get_string('registered', 'grouptool')
+            );
+        }
+
+        return new overviewitem(
+            name: get_string('registration_details', 'grouptool'),
+            value: true,
+            content: get_string('not_registered', 'grouptool')
         );
     }
 
