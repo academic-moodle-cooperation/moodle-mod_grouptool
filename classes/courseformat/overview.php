23257492124d5339228ec790bf70e830e9f668c4
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-04-29 11:06:11 +0200

    8614: Add overview items

--- classes/courseformat/overview.php
+++ classes/courseformat/overview.php
@@ -1,19 +1,4 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
 namespace mod_grouptool\courseformat;
 
 use cm_info;
@@ -36,9 +21,7 @@ use mod_grouptool\dates;
 class overview extends activityoverviewbase {
     /** @var \mod_grouptool $grouptool the grouptool instance. */
     private \mod_grouptool $grouptool;
-    /**
-     * @var renderer_helper
-     */
+    /** @var renderer_helper */
     protected readonly renderer_helper $rendererhelper;
 
     /**
