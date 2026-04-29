f686b1e11283ffb19d045978a6f05ab20318faef
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-04-29 12:07:10 +0200

    8614: Run cbf

--- classes/courseformat/overview.php
+++ classes/courseformat/overview.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 namespace mod_grouptool\courseformat;
 
 use cm_info;
@@ -75,6 +90,7 @@ class overview extends activityoverviewbase {
             content: userdate($duedate),
         );
     }
+    #[\Override]
     public function get_actions_overview(): ?overviewitem {
         if (!has_capability('mod/grouptool:preview', $this->context)) {
             return null;
