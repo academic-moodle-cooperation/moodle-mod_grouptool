6d8d001f434a65dc0890e2c5be6d68929d5fd93d
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-05-04 11:13:15 +0200

    8566: Add change of tab for registration in navigation

--- view.php
+++ view.php
@@ -167,8 +167,19 @@ if ($node) {
         $node2->make_active();
     }
 }
-
-if ($tab != 'selfregistration') {
+if ($tab == 'selfregistration') {
+    $node = $PAGE->navigation->find_active_node();
+    if ($node) {
+        $node->make_inactive();
+        $node3 = match ($tab) {
+            'selfregistration' => $PAGE->navigation->find("mod_grouptool_selfregistration", null),
+            default => false,
+        };
+        if ($node3) {
+            $node3->make_active();
+        }
+    }
+} else {
     // Output starts here!
     echo $OUTPUT->header();
     echo $outputcache;
