f8e20aea42f5ebbfaf5f5fdee86fad242dfb8772
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-05-04 11:13:39 +0200

    8566: Mark groupregistration page in navigation

--- groupregistrations.php
+++ groupregistrations.php
@@ -99,10 +99,20 @@ if ($node) {
     $node->make_inactive();
 }
 
-$node2 = $PAGE->secondarynav->find("mod_grouptool_registration", navigation_node::TYPE_SETTING);
+$node2 = $PAGE->secondarynav->find('mod_grouptool_registration', navigation_node::TYPE_SETTING);
 if ($node2) {
     $node2->make_active();
 }
+
+$node = $PAGE->navigation->find_active_node();
+if ($node) {
+    $node->make_inactive();
+}
+$node3 = $PAGE->navigation->find('mod_grouptool_registration', null);
+if ($node3) {
+    $node3->make_active();
+}
+
 $instance = new mod_grouptool($cm->id, $grouptool, $cm, $course, $context);
 $header = $OUTPUT->header();
 echo $header;
