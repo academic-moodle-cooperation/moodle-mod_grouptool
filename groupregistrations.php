430e3f640dd7faa10752a4a442d74ab8ead2338b
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-05-05 13:09:39 +0200

    8566: Update groupregistrations site so that right tab is marked

--- groupregistrations.php
+++ groupregistrations.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Displays a particular page of mod_grouptool
+ * Group registrations page for mod_grouptool.
  *
  * @package   mod_grouptool
  * @author    Anne Kreppenhofer
@@ -29,9 +29,9 @@ if (((isset($_POST['tab']) && $_POST['tab'] === 'import') || (isset($_GET['tab']
 ) {
     // @codingStandardsIgnoreLine
     define('NO_OUTPUT_BUFFERING', true);
-// @codingStandardsIgnoreLine
 }
-require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+
+require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/mod/grouptool/locallib.php');
 require_once($CFG->dirroot . '/mod/grouptool/definitions.php');
 require_once($CFG->libdir . '/formslib.php');
@@ -44,13 +44,11 @@ require_once($CFG->libdir . '/pdflib.php');
 
 defined('MOODLE_INTERNAL') || die();
 
-global $SESSION, $OUTPUT, $CFG, $DB, $USER, $PAGE;
+global $SESSION, $OUTPUT, $CFG, $DB, $PAGE;
 
-
-// Do we get course_module ID?
 $id = optional_param('id', 0, PARAM_INT);
-// Or do we get grouptool instance ID?
 $g = optional_param('g', 0, PARAM_INT);
+$tab = optional_param('tab', 'overview', PARAM_ALPHA);
 
 if ($id) {
     $cm = get_coursemodule_from_id('grouptool', $id, 0, false, MUST_EXIST);
@@ -62,16 +60,18 @@ if ($id) {
     $cm = get_coursemodule_from_instance('grouptool', $grouptool->id, $course->id, false, MUST_EXIST);
     $id = $cm->id;
 } else {
-    throw new \moodle_exception('invalidcoursemodule');
+    throw new moodle_exception('invalidcoursemodule');
 }
 
 require_login($course, true, $cm);
+
 $context = context_module::instance($cm->id);
-// Hide intro if module not available yet and alwaysshowdescription is false.
-if ($grouptool->alwaysshowdescription == 0 && time() < $grouptool->timeavailable) {
+
+// Hide intro if the module is not available yet and the description should not always be shown.
+if (empty($grouptool->alwaysshowdescription) && time() < $grouptool->timeavailable) {
     $grouptool->intro = '';
 }
-// Configure the page header!
+
 $PAGE->set_url('/mod/grouptool/groupregistrations.php', ['id' => $cm->id]);
 $PAGE->set_context($context);
 $PAGE->set_title(format_string($grouptool->name));
@@ -79,54 +79,54 @@ $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_activity_record($grouptool);
 $PAGE->add_body_class('limitedwidth');
 
-// Mark as viewed!
+// Mark this activity as viewed.
 $completion = new completion_info($course);
 $completion->set_module_viewed($cm);
 
-// Build site according to user capability.
-$viewreggv = has_capability('mod/grouptool:view_regs_group_view', $context);
+$canviewregistrations = has_capability('mod/grouptool:view_regs_group_view', $context);
 
-
-if (!isset($SESSION->mod_grouptool)) {
+if (empty($SESSION->mod_grouptool)) {
     $SESSION->mod_grouptool = new stdClass();
 }
 
 $modinfo = get_fast_modinfo($course);
 $cm = $modinfo->get_cm($cm->id);
 
-$node = $PAGE->secondarynav->find_active_node();
-if ($node) {
-    $node->make_inactive();
+// Mark the correct secondary navigation nodes as active.
+if ($activenode = $PAGE->secondarynav->find_active_node()) {
+    $activenode->make_inactive();
 }
 
-$node2 = $PAGE->secondarynav->find('mod_grouptool_registration', navigation_node::TYPE_SETTING);
-if ($node2) {
-    $node2->make_active();
+if ($registrationnode = $PAGE->secondarynav->find('mod_grouptool_registration', navigation_node::TYPE_SETTING)) {
+    $registrationnode->make_active();
 }
 
-$node = $PAGE->navigation->find_active_node();
-if ($node) {
-    $node->make_inactive();
-}
-$node3 = $PAGE->navigation->find('mod_grouptool_registration', null);
-if ($node3) {
-    $node3->make_active();
+
+if ($mainnode = $PAGE->navigation->find('mod_grouptool_registration', null)) {
+    $mainnode->make_active();
+
+    if (($groupcreationnode = $mainnode->find('mod_grouptool_import', null)) && $tab === 'import') {
+        $groupcreationnode->make_active();
+    }
+    if (($groupcreationnode = $mainnode->find('mod_grouptool_unregister', null)) && $tab === 'unregister') {
+        $groupcreationnode->make_active();
+    }
 }
 
 $instance = new mod_grouptool($cm->id, $grouptool, $cm, $course, $context);
-$header = $OUTPUT->header();
-echo $header;
 
-$tab = optional_param('tab', null, PARAM_ALPHAEXT);
-if (!($viewreggv)) {
+if (!$canviewregistrations) {
     $SESSION->mod_grouptool->currenttab = 'noaccess';
     $tab = 'noaccess';
 }
+
 $url = new moodle_url('/mod/grouptool/groupregistrations.php', ['id' => $cm->id]);
 $options = [
     'import' => get_string('import'),
     'unregister' => get_string('unregister', 'grouptool'),
 ];
+
+echo $OUTPUT->header();
 
 switch ($tab) {
     case 'import':
@@ -134,20 +134,29 @@ switch ($tab) {
             $select = new single_select($url, 'tab', $options, 'import', false);
             echo html_writer::tag('div', $OUTPUT->render($select), ['class' => 'grouptool_manage_user_select']) . '<br>';
         }
+
         $instance->view_import();
         break;
+
     case 'unregister':
         if (has_capability('mod/grouptool:administrate_registration', $context)) {
             $select = new single_select($url, 'tab', $options, 'unregister', false);
             echo html_writer::tag('div', $OUTPUT->render($select), ['class' => 'grouptool_manage_user_select']) . '<br>';
         }
+
         $instance->view_unregister();
         break;
+
     case 'noaccess':
-        $notification = $OUTPUT->notification(get_string('noaccess', 'grouptool'), 'error');
-        echo $OUTPUT->box($notification, 'generalbox centered');
+        echo $OUTPUT->box(
+            $OUTPUT->notification(get_string('noaccess', 'grouptool'), 'error'),
+            'generalbox centered'
+        );
         break;
+
     default:
         $instance->view_overview();
+        break;
 }
+
 echo $OUTPUT->footer();
